6d5956b633e3e4d358c3db69175879d08c65fa7f
ssanya942 <83265366+ssanya942@users.noreply.github.com>
2023-07-14 01:49:58 +0530

    Update metrics.py

--- metrics.py
+++ metrics.py
@@ -23,9 +23,9 @@ def calculate_metrics(ground_truth_dir, prediction_dir):
     ground_truth_files.sort()
     prediction_files.sort()
 
-    # List to store recall and precision arrays without NaN
-    filtered_recall_values = []
-    filtered_precision_values = []
+    pred_sum_pertile = []
+    gt_sum_pertile = []
+    min_val_sum_pertile=[]
 
     # Process each matching ground truth and prediction file
     for i, (ground_truth_file, prediction_file) in enumerate(zip(ground_truth_files, prediction_files), start=1):
@@ -37,7 +37,7 @@ def calculate_metrics(ground_truth_dir, prediction_dir):
         dataset = rasterio.open(ground_truth_path)
 
         # Read the ground truth data into a NumPy array
-        ground_truth_array = dataset.read()
+        ground_truth_array = dataset.read() 
         ## unit conversion
         ground_truth_arr = ground_truth_array * 0.09
 
@@ -49,47 +49,25 @@ def calculate_metrics(ground_truth_dir, prediction_dir):
 
         # Read the prediction data into a NumPy array
         prediction_array = dataset.read()
-        prediction_arr = prediction_array
+        prediction_arr = prediction_array * 0.09
 
         # If the image has multiple bands, you can access each band individually
         prediction_band = prediction_array[0]
 
         min_arr = np.minimum(ground_truth_arr, prediction_arr)
+        # Append  values to the list
+        min_val_sum_pertile.append(np.sum(np.nan_to_num(min_arr)))
+        pred_sum_pertile.append(np.sum(np.nan_to_num(prediction_arr)))
+        gt_sum_pertile.append(np.sum(np.nan_to_num(ground_truth_arr))) 
 
-        # Calculate recall array
-        recall = np.sum(np.nan_to_num(min_arr))/np.sum(np.nan_to_num(ground_truth_arr))
-
-        # Flatten the recall array
-        flattened_recall = recall.flatten()
-
-        # Remove NaN values
-        filtered_recall = flattened_recall[~np.isnan(flattened_recall)]
-
-        # Append the filtered recall values to the list
-        filtered_recall_values.extend(filtered_recall)
-
-        # Calculate precision array
-        precision = np.sum(np.nan_to_num(min_arr))/np.sum(np.nan_to_num(prediction_arr))
-
-        # Flatten the precision array
-        flattened_precision = precision.flatten()
-
-        # Remove NaN values
-        filtered_precision = flattened_precision[~np.isnan(flattened_precision)]
-
-        # Append the filtered precision values to the list
-        filtered_precision_values.extend(filtered_precision)
-
-       
+    recall=np.sum(min_val_sum_pertile)/np.sum(gt_sum_pertile)
+    precision=np.sum(min_val_sum_pertile)/np.sum(pred_sum_pertile)
 
     # Calculate the average recall and precision based on non-NaN values
-    average_recall = np.mean(filtered_recall_values)
-    average_precision = np.mean(filtered_precision_values)
-
-    print("Overall Average Recall:", average_recall)
-    print("Overall Average Precision:", average_precision)
-
+    print("Recall:", recall)
+    print("Precision:", precision)
+    
     return {
-        "recall": average_recall,
-        "precision": average_precision,
+        "recall": recall,
+        "precision": precision,
     }
