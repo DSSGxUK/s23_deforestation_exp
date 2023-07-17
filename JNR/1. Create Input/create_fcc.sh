81e7d6e7a9a8920b6319f8ed4318e0a92aa23724
karan-uppal3 <57282321+karan-uppal3@users.noreply.github.com>
2023-07-17 15:20:15 +0100

    Update JNR/1. Create Input/create_fcc.sh
    Co-authored-by: Jack Buckingham <JackBuck@users.noreply.github.com>

--- JNR/1. Create Input/create_fcc.sh
+++ JNR/1. Create Input/create_fcc.sh
@@ -19,4 +19,4 @@ gdal_calc.py -A $lossyear_map -B $forestcover_map --calc="(A>${end_year})*3 + (B
 gdal_calc.py -A $out_dir/tmp.tif --calc="(A>=3)*3 + (A<3)*A" --NoDataValue=0 --hideNoData --overwrite --outfile $out_dir/merged_map_fcc-123_${start_year}-${end_year}.tif
 
 # Remove the temporary file
-rm -rf $out_dir/tmp.tif
+rm $out_dir/tmp.tif
